19b0eda9b575a2003a4c9a08df396c212d50f1fa
Justin Kim <jjkim928@gmail.com>
2017-08-03 18:01:07 -0400

    shuffle function tweaking

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
-// let container = document.getElementById('container');
-// let arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
-// let randomizedArray = [];
-// function shuffle() {
-//  	for (let i = 0; i < arr.length - 1; i++ ) {
-//    		randomizedArray.push(arr.splice(Math.floor(Math.random()*arr.length), 1)[0]);
-//     }
-//   // Push the remaining item onto tempArr
-// 	randomizedArray.push(arr[0]);
-// 	arr=randomizedArray;
-// 	console.log(arr);
-// }
-// shuffle();
+let initial_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'];
+let memory_values = [];
+let memory_tile_ids = [];
 
-let cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
-// this shuffle array comes from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+// keeps track of tiles flipped
+let tiles_flipped = 0;
+
+
+// adjusted from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    // I still have no idea how to interpret this block below
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
-console.log(shuffleArray(cards));
 
-
+// beginning of the game
+function newGame(){
+	tiles_flipped = 0;
+	let output = '';
+    shuffle(initial_array);
+	for (let i = 0; i < initial_array.length; i++) {
+		//output += '<div id="tile_' + i + '" onclick="memoryFlipTile(this, \'' +initial_array[i] + '\')"></div>';
+		output += '<div id="tile_' 
+		+ i + 
+		'" onclick="memoryFlipTile(this, \' + initial_array[i] + \')"></div>';
+		console.log(output);
+	}
+	document.getElementById('memory_board').innerHTML = output;
+}
+newGame();
 
 
